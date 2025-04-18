1e17d988fa1a5a5c7198088aa196730d35a03260
Pavlo Chernichenko <pwned7067@gmail.com>
2025-04-18 09:05:50 +0300

    Added basic folders

---
+++ src/components/App/App.jsx
@@ -0,0 +1,5 @@
+import css from './App.module.css';
+
+export default function App() {
+  return <main className={css}>Hi team</main>;
+}
---
+++ src/layouts/Header/Header.jsx
@@ -0,0 +1,9 @@
+import Container from '../../ui/Container/Container';
+
+export default function Header() {
+  return (
+    <div>
+      <Container>Header</Container>
+    </div>
+  );
+}
