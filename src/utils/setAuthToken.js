2a8d0a427ccd39b8be151326f200d365900e2b78
Pavlo Chernichenko <pwned7067@gmail.com>
2025-04-18 17:33:25 +0300

    Added a function to add a token to the headers

---
+++ src/utils/setAuthToken.js
@@ -0,0 +1,9 @@
+import { api } from '../service';
+
+export const setAuthToken = token => {
+  if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common['Authorization'];
+  }
+};
