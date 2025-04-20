2048bd1878e5abd9aebae956dba721d5668f35f0
Pavlo Chernichenko <pwned7067@gmail.com>
2025-04-20 20:32:18 +0300

    Added Modal component for modal windows

---
+++ src/ui/Modal/Modal.jsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+import { IoCloseOutline } from 'react-icons/io5';
+import { useMediaPoints } from '../../hooks/useMediaPoints';
+import Container from './../Container/Container';
+import css from './Modal.module.css';
+
+export default function Modal({ children, closeFn, className }) {
+  const { isMobile } = useMediaPoints();
+  return (
+    <div className={css.overlay} onClick={closeFn}>
+      <div className={clsx(css.modal, className)} onClick={e => e.stopPropagation()}>
+        {!isMobile && (
+          <button type="button" onClick={closeFn} className={css.closeBtn}>
+            <IoCloseOutline size={32} />
+          </button>
+        )}
+        {isMobile ? <Container>{children}</Container> : children}
+      </div>
+    </div>
+  );
+}
