6b2f69ce4f2a5ecf524fd96aaf914f8bf593c182
Pavlo Chernichenko <pwned7067@gmail.com>
2025-04-20 21:09:01 +0300

    Added support for closing a modal window on Esc

--- src/ui/Modal/Modal.jsx
+++ src/ui/Modal/Modal.jsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { useEffect } from 'react';
 import { IoCloseOutline } from 'react-icons/io5';
 import { useMediaPoints } from '../../hooks/useMediaPoints';
 import Container from './../Container/Container';
@@ -6,6 +7,20 @@ import css from './Modal.module.css';
 
 export default function Modal({ children, closeFn, className }) {
   const { isMobile } = useMediaPoints();
+
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.key === 'Escape') {
+        closeFn();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [closeFn]);
+
   return (
     <div className={css.overlay} onClick={closeFn}>
       <div className={clsx(css.modal, className)} onClick={e => e.stopPropagation()}>
