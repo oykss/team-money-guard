4a51fa51afe2f209c2bf9cdd526570c0890908fa
Pavlo Chernichenko <pwned7067@gmail.com>
2025-04-21 23:23:38 +0300

    Deleted Persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,16 @@ import { createRoot } from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 import App from './components/App/App.jsx';
-import { persistor, store } from './store/store.js';
+import { store } from './store/store.js';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
     <Toaster position="top-right" reverseOrder={false} />
   </StrictMode>
