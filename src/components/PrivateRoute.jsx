050b24a3929e430e4974e1117260b78f0d59bf92
Pavlo Chernichenko <pwned7067@gmail.com>
2025-04-21 23:22:05 +0300

    Updated Private and Restricted Routes

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,9 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import { selectIsLoggedIn } from './../store/auth/operations';
+import { ROUTES } from '../constants';
+import { selectIsLoggedIn } from '../store/auth/selectors';
 
-export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
+export const PrivateRoute = ({ component: Component, redirectTo = ROUTES.LOGIN }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return isLoggedIn ? Component : <Navigate to={redirectTo} replace />;
