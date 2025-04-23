5a78396bc32d9b5566c53ae0649049279fec23ca
ABurianenko <121970095+ABurianenko@users.noreply.github.com>
2025-04-23 23:21:31 +0100

    add date selectors

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './auth/slice';
 import categoriesSlice from './categories/slice';
 import transactionsSlice from './transactions/slice';
+import statisticsSlice from './statistics/slice'
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     categories: categoriesSlice,
     transactions: transactionsSlice,
+    statistics: statisticsSlice,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
