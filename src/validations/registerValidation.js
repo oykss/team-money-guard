1a66c320d5ac046050e7802b97177b57fdf2e310
Khrystyna Storozhuk <khrystynastorozhuk72@gmail.com>
2025-04-23 19:38:05 -0400

    changed validationsSchemas

--- src/validations/registerValidation.js
+++ src/validations/registerValidation.js
@@ -2,12 +2,16 @@ import * as Yup from 'yup';
 
 export const registerValidationSchema = Yup.object({
   name: Yup.string()
-    .min(3, 'Name must be at least 3 characters')
-    .required('Name is required'),
-  email: Yup.string().email('Invalid email address').required('Email is required'),
+    .min(3, 'Username should have at least 3 characters')
+    .max(33, 'Username should have at most 33 characters')
+    .required('Username is required'),
+  email: Yup.string()
+    .email('Please enter a valid email address')
+    .max(64, 'Email should have at most 64 characters')
+    .required('Email is required'),
   password: Yup.string()
-    .min(8, 'Password must be at least 8 characters')
-    .max(12, 'Password must not be more than 12 characters')
+    .min(8, 'Password should have at least 8 characters')
+    .max(64, 'Password should have at most 64 characters')
     .required('Password is required'),
   _confirmPassword: Yup.string()
     .oneOf([Yup.ref('password'), null], 'Passwords must match')
