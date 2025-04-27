fe4eef6a22a417fed2e874a540802c87a3dca994
Pavlo Chernichenko <pwned7067@gmail.com>
2025-04-27 18:39:03 +0300

    Added skeleton

--- src/components/CurrencyTab/CurrencyTab.jsx
+++ src/components/CurrencyTab/CurrencyTab.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Skeleton from '../../ui/Skeleton/Skeleton';
 import { getCurrency } from '../../utils/monoApiFn';
 import CurrencyChart from '../CurrencyChart/CurrencyChart';
 import css from './CurrencyTab.module.css';
@@ -14,36 +15,36 @@ export default function CurrencyTab() {
     currency();
   }, []);
 
-  return (
-    currency && (
-      <div className={css.container}>
-        <table>
-          <thead>
-            <tr>
-              <th>Currency</th>
-              <th>Purchase</th>
-              <th>Sale</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>USD</td>
-              <td>{currency.usdBuy}</td>
-              <td>{currency.usdSell}</td>
-            </tr>
-            <tr>
-              <td>EUR</td>
-              <td>{currency.euroBuy}</td>
-              <td>{currency.euroSell}</td>
-            </tr>
-          </tbody>
-        </table>
-        <div className={css.chart}>
-          <CurrencyChart />
-          <span className={css.usd}>{currency.usdBuy}</span>
-          <span className={css.euro}>{currency.euroBuy}</span>
-        </div>
+  return currency ? (
+    <div className={css.container}>
+      <table>
+        <thead>
+          <tr>
+            <th>Currency</th>
+            <th>Purchase</th>
+            <th>Sale</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>USD</td>
+            <td>{currency?.usdBuy}</td>
+            <td>{currency?.usdSell}</td>
+          </tr>
+          <tr>
+            <td>EUR</td>
+            <td>{currency?.euroBuy}</td>
+            <td>{currency?.euroSell}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div className={css.chart}>
+        <CurrencyChart />
+        <span className={css.usd}>{currency?.usdBuy}</span>
+        <span className={css.euro}>{currency?.euroBuy}</span>
       </div>
-    )
+    </div>
+  ) : (
+    <Skeleton className={css.skeleton} />
   );
 }
