cde935aaf5985ea66463098d0a0ef7ad51070a3d
ABurianenko <121970095+ABurianenko@users.noreply.github.com>
2025-04-27 22:19:26 +0100

    add skeleton for loading

--- src/components/StatisticTable/StatisticTable.jsx
+++ src/components/StatisticTable/StatisticTable.jsx
@@ -1,12 +1,18 @@
 import { useSelector } from 'react-redux';
-import { selectSummary } from '../../store/statistics/selectors';
+import { selectIsLoading, selectSummary } from '../../store/statistics/selectors';
 import { formatNumber } from '../../utils/formatNumber';
 import { getCategoryColor } from '../../utils/getCategoryColor';
 import css from './StatisticTable.module.css';
+import Skeleton from '../../ui/Skeleton/Skeleton';
 
 export default function StatisticTable() {
   const summary = useSelector(selectSummary);
+  const isLoading = useSelector(selectIsLoading);
   const { expense = [], totalExpense = 0, totalIncome = 0 } = summary;
+
+  if (isLoading) {
+    return <Skeleton width='100%' height='607px'/>
+  }
 
   if (!expense.length) {
     return <p>No transactions for the selected period</p>;
@@ -41,6 +47,6 @@ export default function StatisticTable() {
           <td>{formatNumber(totalIncome)}</td>
         </tr>
       </tbody>
-    </table>
+    </table>    
   );
 }
